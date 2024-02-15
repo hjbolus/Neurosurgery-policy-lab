74006f62069a2dd5921916e8f0eec13df05474dd
hjbolus <141349319+hjbolus@users.noreply.github.com>
2024-02-15 07:43:48 -0600

    Create summary stats for google reviews.py

---
+++ google reviews webscraping/summary stats for google reviews.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import time
+from datetime import datetime
+
+df = pd.read_excel('/Users/harrisbolus/PycharmProjects/pythonProject/google reviews data 021324.xlsx')
+
+numbers = set(df['number'])
+
+list1 = []
+for number in numbers:
+    data =  df[df['number'] == number]
+    reviews = data['reviews']
+
+    list1.append({
+    'number': number,
+    'single_review': len(data[data['reviews'] == 1]),
+    'three_or_fewer': len(data[data['reviews'] < 4]),
+    'twenty_plus': len(data[data['reviews'] > 20]),
+    'mean': reviews.mean(),
+    'median': reviews.median(),
+    'stdev': reviews.std()
+    })
+
+pd.DataFrame(list1).to_excel('/Users/harrisbolus/PycharmProjects/pythonProject/data to add.xlsx')
